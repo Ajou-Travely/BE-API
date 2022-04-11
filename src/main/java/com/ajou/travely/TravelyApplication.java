71df735da3c920428cb36854c0708346e528edb4
park0503 <hooo0503@gmail.com>
2022-04-11 19:57:02 +0900

    Initialize Project

---
+++ src/main/java/com/ajou/travely/TravelyApplication.java
@@ -0,0 +1,13 @@
+package com.ajou.travely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajou/travely/TravelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajou.travely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
