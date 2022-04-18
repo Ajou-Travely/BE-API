5eab362056d03efdf974f281032021789b85a043
parksang <hooo0503@gmail.com>
2022-04-18 23:37:07 +0900

    Customer, Schedule, Reservation, Place, Payment Entity 구현

--- src/main/java/com/ajou/travely/repository/JpaTravelRepository.java
+++ src/main/java/com/ajou/travely/repository/JpaTravelRepository.java
@@ -4,17 +4,4 @@ import com.ajou.travely.domain.Travel;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-
-@Repository
-public class JpaTravelRepository implements TravelRepository{
-    private final EntityManager em;
-
-    public JpaTravelRepository(EntityManager em) {
-        this.em = em;
-    }
-
-    @Override
-    public Travel save(Travel travel) {
-        return null;
-    }
-}
+import java.util.Optional;
