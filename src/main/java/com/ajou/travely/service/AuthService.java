4156f0c438334d27b23f1c88a3687708f4c3ee75
ErranderLee <dre12am14@gmail.com>
2022-04-29 01:37:04 +0900

    feat :  - 로그인 된 유저의 아이디 가져오는 메서드 구현

--- src/main/java/com/ajou/travely/service/AuthService.java
+++ src/main/java/com/ajou/travely/service/AuthService.java
@@ -1,8 +1,11 @@
 package com.ajou.travely.service;
 
 import com.ajou.travely.domain.AuthorizationKakao;
+import com.ajou.travely.domain.user.User;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONObject;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,5 +19,11 @@ public class AuthService {
         Long kakaoId = (Long) userInfoFromKakao.get("id");
         JSONObject result = oauth2Service.setSessionOrRedirectToSignUp(kakaoId);
         return result;
+    }
+
+    public Long getUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getDetails();
+        return user.getId();
     }
 }
