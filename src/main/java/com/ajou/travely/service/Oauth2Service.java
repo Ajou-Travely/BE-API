6c1d07dcec765c0a1846e1d7bd70cff45ac80bd0
Jin Hyeok Son <thswlsgur98@gmail.com>
2022-05-22 00:27:09 +0900

    Fix redirect uri

--- src/main/java/com/ajou/travely/service/Oauth2Service.java
+++ src/main/java/com/ajou/travely/service/Oauth2Service.java
@@ -49,7 +49,7 @@ public class Oauth2Service {
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", grantType);
         params.add("client_id", kakaoOauth2ClinetId);
-        params.add("redirect_uri", "https://dev.travely.guide/oauth/kakao/callback");
+        params.add("redirect_uri", frontendRedirectUrl + "oauth/kakao/callback");
         params.add("code", code);
 
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
