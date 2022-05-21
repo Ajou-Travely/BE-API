c72a73873b7ed06d9a96f7af61741e5e64b21b1c
Jin Hyeok Son <thswlsgur98@gmail.com>
2022-05-22 01:34:21 +0900

    fix: get redirect_uri from request origin header

--- src/main/java/com/ajou/travely/controller/auth/AuthController.java
+++ src/main/java/com/ajou/travely/controller/auth/AuthController.java
@@ -6,12 +6,14 @@ import com.ajou.travely.service.AuthService;
 
 import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONObject;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 @RestController
@@ -20,8 +22,9 @@ public class AuthController {
     private final AuthService authService;
 
     @GetMapping("/v1/oauth2/authorization/kakao")
-    public JSONObject login(@RequestParam("code") String code) {
-        return authService.kakaoAuthentication(code);
+    public JSONObject login(HttpServletRequest request, @RequestParam("code") String code) {
+        String origin = request.getHeader(HttpHeaders.ORIGIN);
+        return authService.kakaoAuthentication(origin, code);
     }
 
     @GetMapping("/v1/isLogin")
