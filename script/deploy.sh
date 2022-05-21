4e2f092a31d2d484ef7732d7b87ce287e5c962bc
Jin Hyeok Son <thswlsgur98@gmail.com>
2022-05-21 21:54:37 +0900

    fix codedeploy

--- script/deploy.sh
+++ script/deploy.sh
@@ -17,4 +17,4 @@ else
 fi
 
 echo "> $JAR_PATH 배포" #3
-nohup java -jar /home/ec2-user/app/build/libs/travely-1.0.jar --spring.config.location=/home/ec2-user/application.yml > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar /home/ec2-user/app/build/libs/travely-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
