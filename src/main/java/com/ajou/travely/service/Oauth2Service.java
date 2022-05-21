523abb6ecf038006fc764146f860f15b4dbe3ca8
Jin Hyeok Son <thswlsgur98@gmail.com>
2022-05-22 01:44:48 +0900

    fix: get redirect_uri from request origin header

--- src/main/java/com/ajou/travely/service/Oauth2Service.java
+++ src/main/java/com/ajou/travely/service/Oauth2Service.java
@@ -49,7 +49,7 @@ public class Oauth2Service {
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", grantType);
         params.add("client_id", kakaoOauth2ClinetId);
-        params.add("redirect_uri", origin + "oauth/kakao/callback");
+        params.add("redirect_uri", origin + "/oauth/kakao/callback");
         params.add("code", code);
 
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
