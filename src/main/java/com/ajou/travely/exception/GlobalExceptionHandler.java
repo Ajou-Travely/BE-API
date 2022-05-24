60d079ca8bf1073f9f18e93a9616477c30c82a3d
parksang <hooo0503@gmail.com>
2022-05-25 01:35:37 +0900

    feat: create exception handler of UnauthorizedException

--- src/main/java/com/ajou/travely/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ajou/travely/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.ajou.travely.exception;
 
 import com.ajou.travely.exception.custom.RecordNotFoundException;
+import com.ajou.travely.exception.custom.UnauthorizedException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,15 +11,21 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(NoHandlerFoundException.class)
-    public ResponseEntity<ErrorResponse> handleNoHandlerFoundException(NoHandlerFoundException ex) {
-        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.API_NOT_FOUND);
-        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
-    }
-
     @ExceptionHandler(RecordNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleRecordNotFoundException(RecordNotFoundException ex) {
         ErrorResponse errorResponse = new ErrorResponse(ex.getErrorCode());
+        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<ErrorResponse> handleUnauthorizedException(UnauthorizedException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getErrorCode());
+        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoHandlerFoundException(NoHandlerFoundException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.API_NOT_FOUND);
         return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
     }
 
