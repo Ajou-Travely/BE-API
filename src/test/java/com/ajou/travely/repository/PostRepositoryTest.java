abdfcaa38c2401ee32eb0e5b6b57d618f883dffd
ErranderLee <dre12am14@gmail.com>
2022-05-30 03:15:16 +0900

    fix:  - remove PostRepositoryTest

--- src/test/java/com/ajou/travely/repository/PostRepositoryTest.java
+++ src/test/java/com/ajou/travely/repository/PostRepositoryTest.java
@@ -1,51 +1,51 @@
-package com.ajou.travely.repository;
-
-import com.ajou.travely.domain.Place;
-import com.ajou.travely.domain.Post;
-import com.ajou.travely.domain.Schedule;
-import com.ajou.travely.domain.travel.Travel;
-import com.ajou.travely.domain.user.UserType;
-import com.ajou.travely.domain.user.User;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import javax.transaction.Transactional;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-@SpringBootTest(properties = {
-        "auth.kakaoOauth2ClinetId=test",
-        "auth.frontendRedirectUrl=test",
-        "spring.mail.password=temptemptemptemp"
-})
-@Transactional
-public class PostRepositoryTest {
-
-    @Autowired
-    private TravelRepository travelRepository;
-
-    @Autowired
-    private PostRepository postRepository;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private ScheduleRepository scheduleRepository;
-
-    @Autowired
-    private PlaceRepository placeRepository;
-
-    public Travel travel;
-    public Place place;
-    public User user;
-    public Schedule schedule;
-
+//package com.ajou.travely.repository;
+//
+//import com.ajou.travely.domain.Place;
+//import com.ajou.travely.domain.Post;
+//import com.ajou.travely.domain.Schedule;
+//import com.ajou.travely.domain.travel.Travel;
+//import com.ajou.travely.domain.user.UserType;
+//import com.ajou.travely.domain.user.User;
+//
+//import java.time.LocalDate;
+//import java.time.LocalDateTime;
+//import javax.transaction.Transactional;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.annotation.Rollback;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//@SpringBootTest(properties = {
+//        "auth.kakaoOauth2ClinetId=test",
+//        "auth.frontendRedirectUrl=test",
+//        "spring.mail.password=temptemptemptemp"
+//})
+//@Transactional
+//public class PostRepositoryTest {
+//
+//    @Autowired
+//    private TravelRepository travelRepository;
+//
+//    @Autowired
+//    private PostRepository postRepository;
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    private ScheduleRepository scheduleRepository;
+//
+//    @Autowired
+//    private PlaceRepository placeRepository;
+//
+//    public Travel travel;
+//    public Place place;
+//    public User user;
+//    public Schedule schedule;
+//
 //    @BeforeEach
 //    public void setUp() {
 //        travel = travelRepository.save(Travel.builder()
@@ -74,46 +74,46 @@ public class PostRepositoryTest {
 //                .place(place)
 //                .build());
 //    }
-
-    @Rollback
-    @Test
-    void 포스트_생성_성공() {
-        // given
-        Post post = Post.builder()
-                .schedule(schedule)
-                .user(user)
-                .text("post content")
-                .title("post title")
-                .build();
-
-        // when
-        Post result = postRepository.save(post);
-
-        // then
-        assertThat(result.getId()).isNotNull();
-        assertThat(result.toString()).isEqualTo(post.toString());
-    }
-
-    @Rollback
-    @Test
-    void 포스트_내용_수정() {
-        // given
-        Post post = Post.builder()
-                .schedule(schedule)
-                .user(user)
-                .text("post content")
-                .title("post title")
-                .build();
-        String updateTitle = "post title2";
-        String updateText = "post content2";
-
-        // when
-        post.update(updateTitle, updateText);
-
-        // then
-        assertThat(post.getTitle()).isEqualTo(updateTitle);
-        assertThat(post.getText()).isEqualTo(updateText);
-
-    }
-
-}
+//
+//    @Rollback
+//    @Test
+//    void 포스트_생성_성공() {
+//        // given
+//        Post post = Post.builder()
+//                .schedule(schedule)
+//                .user(user)
+//                .text("post content")
+//                .title("post title")
+//                .build();
+//
+//        // when
+//        Post result = postRepository.save(post);
+//
+//        // then
+//        assertThat(result.getId()).isNotNull();
+//        assertThat(result.toString()).isEqualTo(post.toString());
+//    }
+//
+//    @Rollback
+//    @Test
+//    void 포스트_내용_수정() {
+//        // given
+//        Post post = Post.builder()
+//                .schedule(schedule)
+//                .user(user)
+//                .text("post content")
+//                .title("post title")
+//                .build();
+//        String updateTitle = "post title2";
+//        String updateText = "post content2";
+//
+//        // when
+//        post.update(updateTitle, updateText);
+//
+//        // then
+//        assertThat(post.getTitle()).isEqualTo(updateTitle);
+//        assertThat(post.getText()).isEqualTo(updateText);
+//
+//    }
+//
+//}
