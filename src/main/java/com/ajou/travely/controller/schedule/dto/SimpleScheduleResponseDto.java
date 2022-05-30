06366bbd202c11a3bfc311aed8c1c79902587ab2
parksang <hooo0503@gmail.com>
2022-05-31 03:15:44 +0900

    refactor: add PlaceResponseDto to SimpleScheduleResponseDto

--- src/main/java/com/ajou/travely/controller/schedule/dto/SimpleScheduleResponseDto.java
+++ src/main/java/com/ajou/travely/controller/schedule/dto/SimpleScheduleResponseDto.java
@@ -1,5 +1,6 @@
 package com.ajou.travely.controller.schedule.dto;
 
+import com.ajou.travely.controller.place.dto.PlaceResponseDto;
 import com.ajou.travely.controller.place.dto.SimplePlaceResponseDto;
 import com.ajou.travely.controller.user.dto.SimpleUserInfoDto;
 import com.ajou.travely.domain.Schedule;
@@ -11,17 +12,17 @@ import java.util.stream.Collectors;
 
 @Getter
 public class SimpleScheduleResponseDto {
-    private Long scheduleId;
-    private LocalTime startTime;
-    private LocalTime endTime;
-    private SimplePlaceResponseDto place;
-    private List<SimpleUserInfoDto> users;
+    private final Long scheduleId;
+    private final LocalTime startTime;
+    private final LocalTime endTime;
+    private final PlaceResponseDto place;
+    private final List<SimpleUserInfoDto> users;
 
     public SimpleScheduleResponseDto(Schedule entity) {
         this.scheduleId = entity.getId();
         this.startTime = entity.getStartTime();
         this.endTime = entity.getEndTime();
-        this.place = new SimplePlaceResponseDto(entity.getPlace());
+        this.place = new PlaceResponseDto(entity.getPlace());
         this.users = entity
                 .getBranches()
                 .stream()
