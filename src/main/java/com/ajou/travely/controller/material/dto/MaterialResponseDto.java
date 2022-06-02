14bb8bd61023bf4a49d0c483df0310900bd63f67
sbhong <tjdqls1668@ajou.ac.kr>
2022-06-02 23:35:08 +0900

    fix: change material response dto property

--- src/main/java/com/ajou/travely/controller/material/dto/MaterialResponseDto.java
+++ src/main/java/com/ajou/travely/controller/material/dto/MaterialResponseDto.java
@@ -2,16 +2,19 @@ package com.ajou.travely.controller.material.dto;
 
 import com.ajou.travely.controller.user.dto.SimpleUserInfoDto;
 import com.ajou.travely.domain.Material;
-import com.ajou.travely.domain.user.User;
 import lombok.Getter;
 
 @Getter
 public class MaterialResponseDto {
+    private final Long id;
     private final SimpleUserInfoDto userInfo;
     private final String material;
+    private final boolean checked;
 
     public MaterialResponseDto(Material entity) {
+        this.id = entity.getId();
         this.userInfo = new SimpleUserInfoDto(entity.getUser());
         this.material = entity.getMaterial();
+        this.checked = entity.getChecked();
     }
 }
