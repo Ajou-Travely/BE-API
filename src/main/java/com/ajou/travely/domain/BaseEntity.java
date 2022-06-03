eda979451d6b209cd3b82d50834a4f4670a76dc8
parksang <hooo0503@gmail.com>
2022-06-04 06:03:27 +0900

    feat: add createdAt field at PostResponseDto

--- src/main/java/com/ajou/travely/domain/BaseEntity.java
+++ src/main/java/com/ajou/travely/domain/BaseEntity.java
@@ -1,8 +1,11 @@
 package com.ajou.travely.domain;
 
+import lombok.Getter;
+
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 public class BaseEntity {
     protected LocalDateTime createdAt;
