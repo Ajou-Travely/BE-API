49e439886bc9ed5d31a3397d63ef44f7d9b74cf3
parksang <hooo0503@gmail.com>
2022-06-05 01:43:24 +0900

    fix: .

--- src/main/java/com/ajou/travely/controller/travel/dto/TravelDateUpdateRequestDto.java
+++ src/main/java/com/ajou/travely/controller/travel/dto/TravelDateUpdateRequestDto.java
@@ -2,10 +2,12 @@ package com.ajou.travely.controller.travel.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
+@NoArgsConstructor
 @Getter
 public class TravelDateUpdateRequestDto {
     @DateTimeFormat(pattern = "yyyy-MM-dd")
