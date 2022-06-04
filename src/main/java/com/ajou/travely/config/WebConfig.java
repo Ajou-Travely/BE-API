6a9e0bfa60823963b0b1f070a4452db77fa5cc4b
ErranderLee <dre12am14@gmail.com>
2022-06-05 05:05:37 +0900

    - fix: remove useless imports

--- src/main/java/com/ajou/travely/config/WebConfig.java
+++ src/main/java/com/ajou/travely/config/WebConfig.java
@@ -8,10 +8,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.multipart.support.MultipartFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
