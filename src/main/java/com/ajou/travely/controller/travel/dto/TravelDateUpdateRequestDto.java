efeb3d31f5cbc334ccbef255675796a46c5fd36a
parksang <hooo0503@gmail.com>
2022-06-05 01:32:54 +0900

    fix: set date time format at TravelDateUpdateRequestDto

--- src/main/java/com/ajou/travely/controller/travel/dto/TravelDateUpdateRequestDto.java
+++ src/main/java/com/ajou/travely/controller/travel/dto/TravelDateUpdateRequestDto.java
@@ -2,12 +2,15 @@ package com.ajou.travely.controller.travel.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 @Getter
 public class TravelDateUpdateRequestDto {
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
 
     @Builder
